92e5f0be0a12b19550f72c9a23565a51841ccc95
Ireegorik <homer.menson@gmail.com>
2021-06-03 20:48:18 +0300

    Initial commit

---
+++ Frendly.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrendlyNumbers
+{
+    class Frendly
+    {
+        public static void GetFrendlies( int k)
+        {
+            int firstSumm;
+            int secondSumm;
+            for (int i = 220; i <= k; i++)
+            {
+                firstSumm = 0;
+                secondSumm = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0) firstSumm += j;
+                    if (firstSumm > k) break;
+                }
+                if ((firstSumm > i) && (firstSumm <= k))
+                {
+                    for (int j = 1; j < firstSumm; j++)
+                    {
+                        if (firstSumm % j == 0) secondSumm += j;
+                        if (firstSumm > k) break;
+                    }
+                    if (secondSumm == i) Console.WriteLine($"{i} {firstSumm}");
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FrendlyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                Frendly.GetFrendlies(Convert.ToInt32(Console.ReadLine()));
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+                Console.WriteLine("RunTime " + elapsedTime);
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
